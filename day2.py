2e9c560cddabd4268785d522312a42e86d8b2420
Shinigaminai <24736346+Shinigaminai@users.noreply.github.com>
2024-12-04 15:28:25 +0100

    day2 - part2

--- day2.py
+++ day2.py
@@ -1,25 +1,39 @@
 import numpy as np
 
-def is_report_safe(levels: list[int]):
-	dir = levels[0] - levels[1]
-	for i in range(len(levels) - 1):
-		diff = levels[i] - levels[i+1]
-		dist = abs(diff)
-		if dist < 1 or dist > 3:
-			return 0
-		same_dir = diff * dir
-		if same_dir < 1:
-			return 0
+def validate_difference(difference: int, direction: int) -> int:
+	dist = abs(difference)
+	if dist < 1 or dist > 3:
+		return 0
+	same_dir = difference * direction
+	if same_dir < 1:
+		return 0
+	return 1
+
+def is_report_safe(levels: list[int], dampen: int = 0) -> bool:
+	differences = [levels[i-1] - levels[i] for i in range(1, len(levels))]
+	direction = differences[0]
+	for i, difference in enumerate(differences):
+		if validate_difference(difference, direction):
+			continue
+		if dampen > 0:
+			for j in range(len(levels)):
+				new_levels = levels.copy()
+				new_levels.pop(j)
+				if is_report_safe(new_levels, dampen-1):
+					print(f"dampened {levels[j]} in {levels} => {new_levels}")
+					return 1
+		return 0
 	return 1
 
 # read input
 safe_reports = 0
+nr_reports = 0
 with open("day2.input.txt", "r") as f:
 	while line := f.readline():
+		nr_reports += 1
 		levels = list(map(int, line.split()))
-		safe = is_report_safe(levels)
-		safe_reports += safe
-		# print(f"{levels} -> {safe}")
-
-# calculate distance
+		if safe := is_report_safe(levels, dampen=1):
+			safe_reports += 1
+			# print(f"{levels} -> {safe}")
+print(f"read {nr_reports} reports")
 print(safe_reports)
